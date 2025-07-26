36ce0c52d3ca6e8029de11a89d39eafb5c7daf7a
jacob <jacob.a.dyson1@gmail.com>
2025-07-26 13:33:31 -0800

    Update config to use SQLALCHEMY_DATABASE_URI

--- config.py
+++ config.py
@@ -21,5 +21,10 @@ class ProductionConfig:
     # Load environment variables
     load_dotenv()  # Load from .env file if it exists
 
-    DATABASE_URL = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "super secret secrets")
+    print(
+        f"DEBUG: SQLALCHEMY_DATABASE_URI = {SQLALCHEMY_DATABASE_URI}"
+    )  # This will show in Render logs
+
     CACHE_TYPE = "SimpleCache"
